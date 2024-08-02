7c4929330a5612985f117f3be8b55c65a8795a9d
Supreme Ciento <35246400+CloudGang@users.noreply.github.com>
2024-08-02 15:25:01 -0400

    Update sk.py

--- sk.py
+++ sk.py
@@ -1,23 +1,5 @@
-
-
-
 import streamlit as st
+import os
 
-# Create the SQL connection to pets_db as specified in your secrets file.
-conn = st.connection('pets_db', type='sql')
-
-# Insert some data with conn.session.
-with conn.session as s:
-    s.execute('CREATE TABLE IF NOT EXISTS pet_owners (person TEXT, pet TEXT);')
-    s.execute('DELETE FROM pet_owners;')
-    pet_owners = {'jerry': 'fish', 'barbara': 'cat', 'alex': 'puppy'}
-    for k in pet_owners:
-        s.execute(
-            'INSERT INTO pet_owners (person, pet) VALUES (:owner, :pet);',
-            params=dict(owner=k, pet=pet_owners[k])
-        )
-    s.commit()
-
-# Query and display the data you inserted
-pet_owners = conn.query('select * from pet_owners')
-st.dataframe(pet_owners)
+# Everything is accessible via the st.secrets dict:
+st.write("DB username:", st.secrets["DB_HOST"])

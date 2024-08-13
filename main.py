44d1825b187ea757b73f80412d847d11e0579517
Supreme Ciento <35246400+CloudGang@users.noreply.github.com>
2024-08-13 10:59:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -119,8 +119,7 @@ loc = get_geolocation()
 with st.sidebar.form(key="my_form"):
     # Radio button for role selection
     role = st.radio("I am a", ["Renter :open_hands:", "Lender :hammer_and_pick:"])
-    refr = st.button("Refresh")
-    if refr:
+    if st.button("Refresh"):
         st.rerun()
     # User input fields
     username = st.text_input("Username")
